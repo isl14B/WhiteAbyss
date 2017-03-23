dcc4b36b22cb665d0b541558eb9df7539aed16e5
chestermx <chestermx88@gmail.com>
2017-03-23 15:23:55 +0900

    [update]topとresult画面の追加

--- general/views.py
+++ general/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+
+class TopView(generic.TemplateView):
+    template_name = "top.html"
+
+
+class ResultView(generic.TemplateView):
+    template_name = "result.html"
