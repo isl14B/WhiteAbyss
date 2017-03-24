6220bbaf5ce3b7a26eeefa311abe9607fee6f4de
howmuch515 <adiura0515@yahoo.co.jp>
2017-03-24 17:46:15 +0900

    [update]complete plugin_detection beta

--- WhiteAbyss/static/WhiteAbyss/js/plugin_detection.js
+++
@@ -1,8 +0,0 @@
-
-document.write("<table border=1><tr><th>name</th><th>description</th><th>filename</th><tr>");
-for(var i=0; i<navigator.plugins.length; i++) {
-    plugin = navigator.plugins[i];
-    document.write("<tr><td>" + plugin.name + "</td><td>" + plugin.description + "</td><td>"  + plugin.filename + "</td><tr>");
-    
-}
-document.write("</table>");
