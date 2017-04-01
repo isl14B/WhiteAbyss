feec46922e8a6a591e115f0a9bcf43e0e456d59b
howmuch515 <adiura0515@yahoo.co.jp>
2017-04-01 20:38:25 +0900

    [fix]add shockwave flash version check function

--- static/WhiteAbyss/js/plugin_detection/main.js
+++ static/WhiteAbyss/js/plugin_detection/main.js
@@ -1,33 +1,45 @@
+// プラグインに対応するバージョンチェック関数をペアで登録しておく
+var VERSION_CHECK_FUNCTION_MAP = {
+    "Shockwave Flash": shockwave_flash
+    }
+    
 function main() {
-    document.write("<table border=1><tr><th>name</th><th>description</th><th>filename</th><tr>");
     var plugin_info_array = [];
     for(var i=0; i<navigator.plugins.length; i++) {
-        plugin = navigator.plugins[i];
-    
-        document.write("<tr><td>" + plugin.name + "</td><td>" + plugin.description + "</td><td>"  + plugin.filename + "</td><tr>");
-        plugin_info_array.push(persePluginInfo(plugin));
+        var plugin = navigator.plugins[i];
+        var plugin_info = extractPluginInfo(plugin);
+        plugin_info_array.push(plugin_info);
     }
-        execPost('./debug_page.html', JSON.stringify(plugin_info_array));
-    document.write("</table>");
+        var result = JSON.stringify(plugin_info_array);
+        document.write(result);
+        execPost('./debug_page.html', result);
 }    
 
 // プラグインの情報からバージョン情報を抽出して、辞書型で返す
-function persePluginInfo(pluginObj) {
-    var plugin_name = pluginObj.name;
-    var plugin_description = pluginObj.description;
-    var plugin_version = ""
+function extractPluginInfo(pluginObj) {
+    console.log(pluginObj);
+    plugin_name = pluginObj.name;
+    plugin_description = pluginObj.description;
+    plugin_version = ""
 
-    if(plugin_name == "Shockwave Flash") {
-       plugin_version =  getVersion_demo(plugin_description);
-    } else {
-        plugin_version = "Not found!";
+    // デバッグ用----------------------------------------------------------------------------------------
+    document.write(plugin_name + "<br>");
+    document.write(plugin_description+ "<br>");
+    document.write("-----------------------------<br>");
+    //----------------------------------------------------------------------------------------------------
+
+    try {
+        plugin_version = VERSION_CHECK_FUNCTION_MAP[plugin_name](plugin_description);
+    } catch (e) {
+        plugin_version = "Not Found!"; // バージョンチェック関数が登録されていない場合
     }
+
     return {name: plugin_name, version: plugin_version};
 }
 
-// ダミーのバージョン識別機能
-function getVersion_demo(description) {
-    return "0xdeadbeef";
+// バージョンチェック関数群
+function shockwave_flash(description) { 
+    return description.match(/\d+\.\d+/)[0];
 }
 
 // データのPOST送信機能
@@ -36,21 +48,21 @@ function getVersion_demo(description) {
  <a onclick="execPost('/hoge', 'data');return false;" href="#">POST送信</a>
 */
 function execPost(action, data) {
- // フォームの生成
- var form = document.createElement("form");
- form.setAttribute("action", action);
- form.setAttribute("method", "post");
- form.style.display = "none";
- document.body.appendChild(form);
- // 送信データの設定
- var input = document.createElement('input');
- input.setAttribute('type', 'hidden');
- input.setAttribute('name', 'plugin_info');
- input.setAttribute('value', data);
- form.appendChild(input);
- 
- // submit
- form.submit();
+    // フォームの生成
+    var form = document.createElement("form");
+    form.setAttribute("action", action);
+    form.setAttribute("method", "post");
+    form.style.display = "none";
+    document.body.appendChild(form);
+    // 送信データの設定
+    var input = document.createElement('input');
+    input.setAttribute('type', 'hidden');
+    input.setAttribute('name', 'plugin_info');
+    input.setAttribute('value', data);
+    form.appendChild(input);
+
+    // submit
+    form.submit();
 }
 
 // MAIN
